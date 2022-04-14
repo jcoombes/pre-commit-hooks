d74c17ab82f804b65b198834b480d1df2898f8b9
Jjazh <coombes.jd@gmail.com>
2022-04-14 01:16:03 +0100

    further progress

--- scratch.py
+++ scratch.py
@@ -6,10 +6,7 @@ import toml
 from loguru import logger as log
 
 BASE_DIR = Path(".")
-pyproject_path = BASE_DIR / "pyproject.toml"
-
-PASS, FAIL = 0, 1
-EXIT_CODE = PASS
+pyproject_path = BASE_DIR / "pyproject5.toml"
 
 
 def insert_comment_line(filename, prod=True):
@@ -22,15 +19,20 @@ def insert_comment_line(filename, prod=True):
     # prod: bool = True
     # Returns None. Reads and writes the filename file.
     if prod:
-        string_before_line = "[tool.poetry.dependencies]\n"
+        string_before_line = "[tool.poetry.dependencies]"
     if not prod:
-        string_before_line = "[tool.poetry.dev-dependencies]\n"
+        string_before_line = "[tool.poetry.dev-dependencies]"
 
     comment_line = "# Everything after this should be sorted alphabetically\n"
     with open(filename, "r") as f:
         contents = f.readlines()
 
-    contents.insert(contents.index(string_before_line) + int(prod), comment_line)
+    log.info(contents)
+
+    line_index_gen = (
+        idx + 1 for idx, line in enumerate(contents) if string_before_line in line
+    )
+    contents.insert(next(line_index_gen) + int(prod), comment_line)
 
     with open(filename, "w") as f:
         f.writelines(contents)
@@ -38,44 +40,68 @@ def insert_comment_line(filename, prod=True):
     return None
 
 
-def main():
-    pyp = toml.load(pyproject_path)
-    unordered_deps = copy.deepcopy(pyp["tool"]["poetry"]["dependencies"])
-    unordered_dev_deps = copy.deepcopy(pyp["tool"]["poetry"]["dev-dependencies"])
-    u_deps, u_dev_deps = OrderedDict(unordered_deps), OrderedDict(unordered_dev_deps)
+def main(pyproject_path):
+    def fix_either_path(pyproject_path, prod=True):
+        pyp = toml.load(pyproject_path)
+        try:
+            pyp["tool"]["poetry"]
+        except KeyError:
+            log.info("Not using poetry - no imports to sort")
+            return False
 
-    ordered_deps = OrderedDict(sorted(unordered_deps.items())).move_to_end(
-        "python", last=False
-    )
-    log.info(ordered_deps)
+        log.info(pyp)
 
-    ordered_dev_deps = OrderedDict(sorted(unordered_dev_deps.items()))
-    log.info(ordered_dev_deps)
+        if prod:
+            try:
+                unordered_deps = copy.deepcopy(pyp["tool"]["poetry"]["dependencies"])
+            except KeyError:
+                log.info("No dependencies specified")
+                return False
 
-    pyp["tool"]["poetry"]["dependencies"] = ordered_deps
-    pyp["tool"]["poetry"]["dev-dependencies"] = ordered_dev_deps
+            ordered_deps = OrderedDict(sorted(unordered_deps.items())).move_to_end(
+                "python", last=False
+            )
+            pyp["tool"]["poetry"]["dependencies"] = ordered_deps
+            log.info(ordered_deps)
+            if OrderedDict(unordered_deps) != ordered_deps:
+                with open(pyproject_path, "w") as f:
+                    toml.dump(pyp, f)
+                insert_comment_line(pyproject_path, prod=True)
+                log.info("Production dependencies sorted")
+                return True
+            else:
+                log.info("Production dependencies unchanged")
+                return False
 
-    if u_deps != ordered_deps and u_dev_deps != ordered_dev_deps:
-        log.info("pyproject.toml modified - Both deps and dev-deps ordered")
-        toml.dump(pyp, pyproject_path)
-        insert_comment_line(pyproject_path, prod=True)
-        insert_comment_line(pyproject_path, prod=False)
-        EXIT_CODE = FAIL
-    elif u_deps != ordered_deps and u_dev_deps == ordered_dev_deps:
-        log.info("pyproject.toml modified - dependencies ordered")
-        toml.dump(pyp, pyproject_path)
-        insert_comment_line(pyproject_path, prod=True)
-        EXIT_CODE = FAIL
-    elif u_deps == ordered_deps and u_dev_deps != ordered_dev_deps:
-        log.info("pyproject.toml modified - dev-dependencies ordered")
-        toml.dump(pyp, pyproject_path)
-        insert_comment_line(pyproject_path, prod=False)
-        EXIT_CODE = FAIL
-    else:
-        log.info("dependencies and dev-dependencies left unchanged.")
+        elif not prod:
+            try:
+                unordered_deps = copy.deepcopy(
+                    pyp["tool"]["poetry"]["dev-dependencies"]
+                )
+            except KeyError:
+                log.info("No dev dependencies specified")
+                return False
 
-    return EXIT_CODE
+            ordered_deps = OrderedDict(sorted(unordered_deps.items()))
+            pyp["tool"]["poetry"]["dev-dependencies"] = ordered_deps
+            log.info(ordered_deps)
+            if OrderedDict(unordered_deps) != ordered_deps:
+                with open(pyproject_path, "w") as f:
+                    toml.dump(pyp, f)
+                insert_comment_line(pyproject_path, prod=False)
+                log.info("Development dependencies sorted")
+                return True
+            else:
+                log.info("Development dependencies unchanged")
+                return False
+
+    # Force the logical OR not to short-circuit.
+    left = fix_either_path(pyproject_path, prod=True)
+    right = fix_either_path(pyproject_path, prod=False)
+
+    # Note that True means the test failed
+    return int(left or right)
 
 
 if __name__ == "__main__":
-    main()
+    main(pyproject_path)
